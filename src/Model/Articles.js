8ac2fcfc5fcfa14c1c2601f296e8164cef0f4d67
Ey_jun <gregoiresenechal@outlook.com>
2024-04-08 15:07:41 +0200

    suppression model.user

--- src/Model/Articles.js
+++ src/Model/Articles.js
@@ -1,11 +1,10 @@
 class Articles {
-  constructor(title, image, description, location, price, user, date) {
+  constructor(title, image, description, location, price, date) {
     this.title = title;
     this.image = image;
     this.description = description;
     this.location = location;
     this.price = price;
-    this.user = user;
     this.date = date;
   }
 }
